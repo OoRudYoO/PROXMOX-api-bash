2fd534daa84b29baf9c62ebd0c20c3d423f15303
OoRudYoO <none@piwnet.pl>
2021-02-24 02:22:26 +0100

    add variable for TARGETNODE

--- container_create.sh
+++ container_create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROXMOX_NODE_IP=ip-here
+TARGETNODE=proxmox-hostname
 
 CPU=1
 CPUUNITS=512
@@ -14,7 +15,7 @@ for i in $(seq 710 720);do
 
     echo " creating container $i ...";
 
-    curl --insecure  --cookie "$(<cookie)" --header "$(<csrftoken)" -X POST --data-urlencode net0="name=tnet$i,bridge=vmbr0" --data-urlencode ostemplate="IMAGES:vztmpl/centos-8-default_20191016_amd64.tar.xz" --data-urlencode storage="DATA" --data vmid=$i --data cores=$CPU --data cpuunits=$CPUUNITS --data memory=$MEMORY --data swap=$SWAP --data hostname=tnode$i https://$PROXMOX_NODE_IP:8006/api2/json/nodes/oldpx/lxc
+    curl --insecure  --cookie "$(<cookie)" --header "$(<csrftoken)" -X POST --data-urlencode net0="name=tnet$i,bridge=vmbr0" --data-urlencode ostemplate="IMAGES:vztmpl/centos-8-default_20191016_amd64.tar.xz" --data-urlencode storage="DATA" --data vmid=$i --data cores=$CPU --data cpuunits=$CPUUNITS --data memory=$MEMORY --data swap=$SWAP --data hostname=tnode$i https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$TARGETNODE/lxc
 
     echo " wait $SLEEP s";
     sleep $SLEEP;
